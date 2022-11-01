3c15d4f32b7468cae7e8d5a7bf06ee39540fa211
shahzamalahmad <shahzamal2013@gmail.com>
2022-11-01 23:52:03 +0530

    form, requirement model,

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   get "articles/about"
   get "articles/services"
-  get "articles/gallery"
+  get "articles/gallery", to: "articles#gallery"
   get "articles/faq"
   get "articles/new", to: "articles#new"
   get "articles/contact"
